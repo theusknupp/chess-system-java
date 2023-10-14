67a0a8832945e87cfb10838e7982b8f96c42154f
theusknupp <matheus.knupp.ds@outlook.com>
2023-10-13 23:19:53 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Olá! Este será meu primeiro projeto! ");
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
